18a835918c20f8b91878916304a84b338d50cd5e
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-04-13 18:20:26 +0530

    Added the welcome message.

---
+++ src/com/bridgelabz/CSVUser.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class CSVUser {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book reading from csv and json");
+    }
+}
